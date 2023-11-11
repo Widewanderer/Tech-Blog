a703001480ac2620746f042cc2b0292dedfbfcf7
Widewanderer <slavatrotz@gmail.com>
2023-11-10 18:44:41 -0600

    aded to quantifiers

--- public/js/comment.js
+++ public/js/comment.js
@@ -13,5 +13,19 @@ commentSubmit.addEventListener("click", async function (event) {
 const content = commentForm.value.trim();
 const post_id = commentForm.getAttribute("data-post_id");
 // send fetch to comment js wt info above content and post_id 
+if (content) {
+    const response = await fetch(`/api/comment`, {
+        method: "POST",
+        body: JSON.stringify({ content, post_id }),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    if (response.ok) {
+        document.location.replace( `/post/${comment}` );
+    } else {
+        alert("Failed to create comment");
+    }
+}
 });
 
