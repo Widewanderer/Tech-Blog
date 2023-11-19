c39d67961cb813dfb7998d8d07b9faf7edf59800
Widewanderer <slavatrotz@gmail.com>
2023-11-19 12:34:54 -0600

    experimenting with fixing .hidden for comments and edit and delete buttons for post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+// Get all posts
 router.post('/', withAuth, async (req, res) => {
   try {
     const newPost = await Post.create({
@@ -15,6 +16,7 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+// Delete a post by its `id` value
 router.delete('/:id', withAuth, async (req, res) => {
   try {
     const postData = await Post.destroy({
@@ -28,13 +30,42 @@ router.delete('/:id', withAuth, async (req, res) => {
       res.status(404).json({ message: 'No post found with this id!' });
       return;
     }
-    //UpdatePost?? 
 
     res.status(200).json(postData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
+// Update a post by its `id` value
+router.put("/:id", withAuth, async (req, res) => {
+  try {
+    const updatedPost = await Post.update(
+      {
+        title: req.body.title,
+        content: req.body.content,
+      },
+      {
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      }
+    );
+
+    // Check if the post was found and updated
+    if (!updatedPost[0]) {
+      res.status(404).json({ message: "No post found with this id!" });
+      return;
+    }
+
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
 
 module.exports = router;
 
